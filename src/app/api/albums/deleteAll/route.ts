ddf32ac0adadde38f04f0d7ea6e0420547084d3f
Ancient <nevionn@yandex.ru>
2025-05-04 02:22:22 +0300

    доработан эндпоинт deleteAll

--- src/app/api/albums/deleteAll/route.ts
+++ src/app/api/albums/deleteAll/route.ts
@@ -1,15 +1,35 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import fs from "fs/promises";
+import path from "path";
 
 const prisma = new PrismaClient();
 
 export async function DELETE() {
   try {
+    const photos = await prisma.photo.findMany({
+      select: { path: true },
+    });
+
+    // Удаляем файлы из папки public/uploads
+    await Promise.all(
+      photos.map(async (photo) => {
+        const filePath = path.join(process.cwd(), "public", photo.path);
+        try {
+          await fs.unlink(filePath);
+        } catch (error) {
+          console.warn(`Файл ${filePath} не найден, пропускаем:`, error);
+        }
+      })
+    );
+
     await prisma.album.deleteMany({});
 
-    return NextResponse.json({ message: "Все альбомы и фотографии удалены" });
+    return NextResponse.json({
+      message: "Все альбомы, фотографии и файлы удалены",
+    });
   } catch (error) {
-    console.error("Ошибка при удалении альбомов:", error);
+    console.error("Ошибка при удалении альбомов и файлов:", error);
     return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
   }
 }
